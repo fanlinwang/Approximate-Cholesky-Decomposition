cc58b95516a92c164275cebd833c30bd8d226746
Fanlin Wang <fanwang@student.ethz.ch>
2020-04-16 11:15:03 +0200

    Add LDLsolver.

---
+++ approxChol.cpp
@@ -0,0 +1,72 @@
+#include "approxCholTypes.h"
+#include <iostream>
+
+void forward(const LDLinv& ldli, std::vector<Tval>& y) {
+    int len = ldli.col.size(); 
+    for (int ii = 0; ii < len-1; ii++){ 
+        Tind i = ldli.col[ii];
+
+        Tind j0 = ldli.colptr[ii];
+        Tind j1 = ldli.colptr[ii+1]-Tind(1);
+
+        Tval yi = y[i];
+
+        for (Tind jj = j0; jj < j1-1; jj++){
+            Tval j = ldli.rowval[jj];
+            y[j] += ldli.fval[jj] * yi;
+            yi *= ((Tval)(1.0)-ldli.fval[jj]);
+        }
+        Tval j = ldli.rowval[j1];
+        y[j] += yi;
+        y[i] = yi;
+    }
+} 
+
+void backward(const LDLinv& ldli, std::vector<Tval>& y) {
+    int len = ldli.col.size(); 
+    for (int ii = len-1; ii >= 0; ii--) {
+        Tind i = ldli.col[ii];
+
+        Tind j0 = ldli.colptr[ii];
+        Tind j1 = ldli.colptr[ii+1]-Tind(1);
+
+        Tval j = ldli.rowval[j1];
+        Tval yi = y[i];
+        yi = yi + y[j];
+
+        for (Tind jj = j1-(Tind)(1.0); jj >= j0; jj--) {
+            j = ldli.rowval[jj];
+            yi = (1-ldli.fval[jj])*yi + ldli.fval[jj]*y[j];
+        }
+        y[i] = yi;
+    }
+} 
+
+Tval mean(std::vector<Tval> y){
+    Tval sum = 0.0;
+    for (auto elem: y){
+        sum += elem;
+    }
+    return sum/y.size();
+}
+
+std::vector<Tval> LDLsolver(const LDLinv& ldli, const std::vector<Tval>& b){
+    std::vector<Tval> y(b.begin(), b.end());
+
+    forward(ldli, y);
+
+    for (int i = 0; i < ldli.d.size(); i++){
+        if (ldli.d[i] != 0)
+            y[i] /= ldli.d[i];
+    }
+
+    backward(ldli, y);
+
+    Tval mu = mean(y);
+    for (auto elem: y){
+        elem = elem - mu;
+    }
+
+    return y;
+}
+
