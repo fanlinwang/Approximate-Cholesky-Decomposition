0b17b2f8ab962d8e058260c5b14cef4479740fde
Daoye Wang <daoyewang@Daoyes-MacBook-Pro.local>
2020-04-22 11:04:03 +0200

    add approxChol function header

--- src/approxChol.hpp
+++ src/approxChol.hpp
@@ -2,6 +2,12 @@
 
 #include "approxCholTypes.h"
 
+void print_ll_col(LLMatOrd llmat, int i);
+int get_ll_col(LLMatOrd llmat, int i, std::vector<LLcol> &colspace);
+bool cmp_row(const LLcol &a, const LLcol &b);
+bool cmp_val(const LLcol &a, const LLcol &b);
+Tind compressCol(std::vector<LLcol> &colspace, int len);
+LDLinv approxChol(LLMatOrd a);
 
 void forward(const LDLinv& ldli, std::vector<Tval>& y);
 void backward(const LDLinv& ldli, std::vector<Tval>& y); 
