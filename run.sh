8c347e2f49b73847374e2d233cefd33069d4b00f
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-06 22:45:07 +0200

    Add a script to test perf.

--- run.sh
+++ run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 #seq FIRST STEP LAST
-vertice=(10 100 1000) 
-edge=(10 100)
+vertice=(10 100) 
+edge=(10 200)
 logfile="./"
 
 for V in ${vertice[@]}
 do
     for E in ${edge[@]} 
     do 
-        ./build/src/main $V $E | tee -a "./log.txt" 
-        echo "========================" | tee -a "./log.txt" 
+	if [ $E -lt $(($V*$V)) ]
+	then
+	    echo "Start test. V = ${V}, E = ${E}" | tee -a "./log.txt"
+	    perf stat -d ./build/src/main $V $E | tee -a "./log.txt"
+            perf stat -e r5302b1,r53010e,r5302c7,r5300c4,r5308c7,r5320c7,r5301c7,r5304c7,r5310c7,r5303c7,r533cc7,r532ac7,r5315c7 ./build/src/main $V $E | tee -a "./log.txt" 
+            echo "========================" | tee -a "./log.txt" 
+	fi    
     done
 done
 
