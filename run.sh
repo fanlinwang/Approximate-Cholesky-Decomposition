6f3555ff6473a0af69a14b07b845abe5f34336e7
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-15 00:21:30 +0200

    Minor.

--- run.sh
+++ run.sh
@@ -9,20 +9,21 @@ for V in ${vertice[@]}
 do
     for E in ${edge[@]}
     do
-        if [[ $E -ge $(($V)) ]] && [[ $E -lt $(($V*$V)) ]]
+        if [[ $E -ge $(($V)) ]] && [[ $E -lt $(($V*10)) ]]
         then
            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/general_metrics.txt"
            3>>"./log/general_metrics.txt" perf stat --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/general_metrics.txt"
 
            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/cache_log.txt"
-           3>>"./log/cache_log.txt" perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,iTLB-loads,iTLB-load-misses --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/cache_log.txt"
+           3>>"./log/cache_log.txt" perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,iTLB-loads,iTLB-load-misses --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/cache_log.txt"
 
            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/flops_log.txt"
            3>>"./log/flops_log.txt" perf stat -e r5301c7,r5302c7 --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/flops_log.txt"
 
            perf record -o "./log/perf_v${V}_e${E}.data" ./build/src/main $V $E
-           2>>"./log/perf_v${V}_e${E}.txt" perf report --stdio --dsos=main -i "./log/perf_v${V}_e${E}.data"
-           1>>"./log/perf_annotate_v${V}_e${E}.txt" perf annotate --stdio --dsos=main --symbol=approxChol -i "./log/perf_v${V}_e${E}.data"
+	   perf report --stdio --dsos=main -i "./log/perf_v${V}_e${E}.data" | tee -a "./log/pf_v${V}_e${E}.txt"
+           perf annotate --stdio --dsos=main -i "./log/perf_v${V}_e${E}.data"  | tee -a "./log/p_v${V}_e${E}.txt"
+           1>>"./log/perf_annotate_v${V}_e${E}.txt" perf annotate --stdio --dsos=main --symbol=approxChol -i "./log/perf_v${V}_e${E}.data"  
         fi
     done
 done
