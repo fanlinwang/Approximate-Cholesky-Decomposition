50bedc4e34030092283c5a18ef57c3cc1c99d86c
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-14 16:46:52 +0200

    Perf.

--- run.sh
+++ run.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 #seq FIRST STEP LAST
-vertice=(10 100 1000)
-edge=(10 100 1000 10000)
+vertice=(10 100 1000 2000 5000 8000 10000 20000 50000)
+edge=(10 100 500 1000 2000 5000 8000 10000 20000 50000 100000)
+
 
 for V in ${vertice[@]}
 do
     for E in ${edge[@]}
     do
-        if [ $E -lt $(($V*$V)) ]
+        if [[ $E -ge $(($V)) ]] && [[ $E -lt $(($V*$V)) ]]
         then
-            echo "Start test. V = ${V}, E = ${E}" | tee -a "./cache_log.txt"
-            3>>cache_log.txt perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-dcache-prefetches,L1-dcache-prefetch-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,LLC-prefetch-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,dTLB-prefetches,dTLB-prefetch-misses,iTLB-loads,iTLB-load-misses --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./cache_log.txt"
-            echo "Start test. V = ${V}, E = ${E}" | tee -a "./flops_log.txt"
-            3>>flops_log.txt perf stat -e r5301c7,r5302c7 --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./flops_log.txt"
-            perf record -o "./perf_v${V}_e${E}.data"
-            3>>"./perf_v${V}_e${E}.data" perf report -i "./perf_v${V}_e${E}.data"
+           echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/general_metrics.txt"
+           3>>"./log/general_metrics.txt" perf stat --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/general_metrics.txt"
+
+           echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/cache_log.txt"
+           3>>"./log/cache_log.txt" perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,iTLB-loads,iTLB-load-misses --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/cache_log.txt"
+
+           echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/flops_log.txt"
+           3>>"./log/flops_log.txt" perf stat -e r5301c7,r5302c7 --log-fd 3 --append -d ./build/src/main $V $E | tee -a "./log/flops_log.txt"
+
+           perf record -o "./log/perf_v${V}_e${E}.data" ./build/src/main $V $E
+           2>>"./log/perf_v${V}_e${E}.txt" perf report --stdio --dsos=main -i "./log/perf_v${V}_e${E}.data"
+           1>>"./log/perf_annotate_v${V}_e${E}.txt" perf annotate --stdio --dsos=main --symbol=approxChol -i "./log/perf_v${V}_e${E}.data"
         fi
     done
 done
