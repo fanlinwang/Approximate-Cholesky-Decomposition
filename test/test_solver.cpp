54b9378192ce6ce32ace4eff813c3b89fea16f58
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-15 09:41:02 +0200

    Add log files.

--- test/test_solver.cpp
+++ test/test_solver.cpp
@@ -3,27 +3,18 @@
 #include "test.h"
 #include "matrix.hpp"
 
-double a[10000];
 
-int main(){
+int main(int argc, char **argv){
 
-    int rep = 100;
-    for (int j = 0; j < rep; ++j) {
-        for (int i = 0; i < 10000; ++i){
-            a[i] = i*0.001;
-            a[i] *= a[i];
-        }
-    }
-/*
     int v = atoi(argv[1]);
-    int e= atoi(argv[2]);
+    int e = atoi(argv[2]);
 
     SparseMatrix A(v, e);
     LLMatOrd llmat = LLMatOrd(A);
     std::cout << "Created random sparse matrix A.\n";
 
-    flops = approxChol_count(llmat);
-*/
+    approxChol(llmat);
+
     return 0;
 
 }
