9e4f2a498c3ed036f6b87b3b34b16da81fb84057
Daoye Wang <daoyewang@Daoyes-MacBook-Pro.local>
2020-05-18 11:29:11 +0200

    clean up

--- src/conditionNumber.cpp
+++ src/conditionNumber.cpp
@@ -5,42 +5,6 @@
 #include <cmath>
 #include <iostream>
 
-SparseMatrix adj2lap(SparseMatrix& a){
-    SparseMatrix la;
-    la.colnum = a.colnum;
-    la.elems = a.elems + a.colnum;
-    la.colptrs = std::vector<Tind>(la.colnum + 1);
-    for (int colidx = 0; colidx < la.colnum; colidx++){
-        la.colptrs.at(colidx) = a.colptrs.at(colidx) + colidx;
-    }
-    la.colptrs.at(la.colnum) = la.elems;
-    la.rows = std::vector<Tind>(la.elems);
-    la.vals = std::vector<Tval>(la.elems);
-    for (int colidx = 0; colidx < la.colnum; colidx++){
-        int i = a.colptrs.at(colidx);
-        int j = la.colptrs.at(colidx);
-        Tval deg = 0.0;
-        bool above_diag = true;
-        Tind diag_place = la.elems-1;
-        while (i < a.elems && i < a.colptrs.at(colidx+1)){
-            if (above_diag){
-                if (a.rows.at(i) > colidx){
-                    above_diag = false;
-                    diag_place = j;
-                    j++; // leave the place for degree, which should be on the diagonal
-                }
-            }
-            deg += a.vals.at(i);
-            la.vals.at(j) = -a.vals.at(i);
-            la.rows.at(j) = a.rows.at(i);
-            i++; j++;
-        }
-        la.rows.at(diag_place) = colidx;
-        la.vals.at(diag_place) = deg;
-    }
-    return la;
-}
-
 void op1(SparseMatrix& a, const LDLinv& ldli, Tval u, std::vector<Tval>& y){
     SparseMatrix la; 
     laplacian(a, la);
@@ -48,68 +12,33 @@ void op1(SparseMatrix& a, const LDLinv& ldli, Tval u, std::vector<Tval>& y){
     for (auto& elem: y){
         elem = elem - mu;
     }
-    std::cout << "after projection: \t";
-    for (auto elem : y ){std::cout << elem << " ";} 
-    std::cout << "\n";
 
-    // std::vector<Tval> sol = LDLsolver(ldli, y);
     std::vector<Tval> ldlsol = LDLsolver(ldli, y);
     SolverParameter para;
     para.verbose = false;
     para.tolerance = 1e-6;
     para.maxits = 1000;
     std::vector<Tval> sol = pcg(la, y, LDLsolver, ldlsol, ldli, para);
-    // SolverParameter para;
-    // para.verbose = true;
-    // para.tolerance = 1e-8;
-    // std::vector<Tval> sol(y.size());
-    // approxchol_lapGiven(a_copy, la, y, sol, para);
-
-    std::cout << "after M^{-1}: \t \t";
-    for (auto elem : sol ){std::cout << elem << " ";} 
-    std::cout << "\n";
 
     y = la * sol;
-
-    std::cout << "L M^{-1}: \t \t";
-    for (auto elem : y ){std::cout << elem << " ";} 
-    std::cout << "\n";
 }
 
 void op2(SparseMatrix& a, const LDLinv& ldli, Tval u, std::vector<Tval>& y){
-    // SparseMatrix la = adj2lap(a);
     SparseMatrix la; 
     laplacian(a, la);
     Tval mu = mean(y);
     for (auto& elem: y){
         elem = elem - mu;
     }
-    std::cout << "after projection: \t";
-    for (auto elem : y ){std::cout << elem << " ";} 
-    std::cout << "\n";
 
-    // std::vector<Tval> sol = LDLsolver(ldli, y);
     std::vector<Tval> ldlsol = LDLsolver(ldli, y);
     SolverParameter para;
     para.verbose = true;
     para.tolerance = 1e-6;
     para.maxits = 1000;
     std::vector<Tval> sol = pcg(la, y, LDLsolver, ldlsol, ldli, para);
-    // SolverParameter para;
-    // para.verbose = true;
-    // para.tolerance = 1e-8;
-    // std::vector<Tval> sol(y.size());
-    // approxchol_lapGiven(a_copy, la, y, sol, para);
-
-    std::cout << "after M^{-1}: \t \t";
-    for (auto elem : sol ){std::cout << elem << " ";} 
-    std::cout << "\n";
 
     sol = la * sol;
-
-    std::cout << "L M^{-1}: \t \t";
-    for (auto elem : sol ){std::cout << elem << " ";} 
-    std::cout << "\n";
 
     for (auto& elem : y){
         elem *= u;
@@ -118,10 +47,6 @@ void op2(SparseMatrix& a, const LDLinv& ldli, Tval u, std::vector<Tval>& y){
     for (int i = 0; i < n; i++){
         y[i] -= sol[i];
     }
-
-    std::cout << "out from the operator: \t";
-    for (auto elem : y ){std::cout << elem << " ";} 
-    std::cout << "\n";
 }
 
 Tval eigs(LinearOperator op, SparseMatrix& la, const LDLinv& ldli, Tval u,
@@ -162,11 +87,6 @@ Tval eigs(LinearOperator op, SparseMatrix& la, const LDLinv& ldli, Tval u,
         eigenvalue = 0.0;
         Tval numerator = 0.0;
         Tval denominator = 0.0;
-        // std::cout << "calculating the eigenvalue:\n";
-        // for (auto elem : x ){std::cout << elem << " ";} 
-        // std::cout << "\n";
-        // for (auto elem : y_unnormalized ){std::cout << elem << " ";} 
-        // std::cout << "\n";
         for (int i = 0; i < n; i++){
             numerator += x.at(i) * y_unnormalized.at(i);
             denominator += x.at(i) * x.at(i);
@@ -178,15 +98,6 @@ Tval eigs(LinearOperator op, SparseMatrix& la, const LDLinv& ldli, Tval u,
             diff[i] = eigenvalue * x[i] - y_unnormalized[i];
         }
         diffnorm = norm(diff);
-
-        //debug
-        for (int i = 0; i < n; i++){std::cout << x[i] << " ";} 
-        std::cout << "\n";
-        for (int i = 0; i < n; i++){std::cout << y_unnormalized[i] << " ";}
-        std::cout << "\n";
-        for (int i = 0; i < n; i++){std::cout << y[i] << " ";}
-        std::cout << "\n eigenvalue: \t\t" << eigenvalue << "\n";
-        std::cout << "Diffnorm: \t\t" << diffnorm << "\n";
     }
 
     return eigenvalue;
@@ -205,7 +116,6 @@ Tval ApproxCholValidation(SparseMatrix& a, const LDLinv& ldli, Tval tol){
 
     // Compute max eigenvalue of A * L^{-1}
     Tval upper = eigs(&op1, a, ldli, 0.0, y, 1, tol);
-    // Tval upper = 1.0;
 
     // initialize eigenvector again for the second operator
     for (int i = 0; i < n; i++){
@@ -216,6 +126,6 @@ Tval ApproxCholValidation(SparseMatrix& a, const LDLinv& ldli, Tval tol){
     
     std::cout << "biggest eigenvalue: " << upper << "\n \n";
     std::cout << "second smallest eigenvalue: " << lower << "\n";
-    // Tval lower = 1;
+    
     return upper / lower;
 }
--- test/test_validation.cpp
+++ test/test_validation.cpp
@@ -19,23 +19,12 @@ int main(){
         {1,1,0,0,0},
         {0,1,0,0,0}};
     // SparseMatrix A(matrix);
+    // SparseMatrix A(matrix2);
     SparseMatrix A(1000, 5000, 1);
-    // std::cout << A;
-    SparseMatrix la = adj2lap(A);
-    std::cout << la;
     LLMatOrd llmat = LLMatOrd(A);
 
     LDLinv ldli = approxChol(llmat);
-    std::cout << ldli << std::endl;
-
-    // std::vector<Tval> b = {0.3777397416887483, -1.012775866312444, 
-    //                        1.017599128056379, -0.38256300343268407};
-    // std::vector<Tval> b = {0.3, 0.2, -0.1, -0.4};
-    // std::vector<Tval> b = {0.3, 0.2, 0.2, -0.1, -0.6};
-    // std::vector<Tval> b = {1.0, 1.0, 1.0, 1.0, 1.0};
-    // std::vector<Tval> sol = LDLsolver(ldli, b);
-    std::vector<Tval> b = {-0.46397, 0.0910944, -0.572403, 0.504353, 0.440926};
-
+    // std::cout << ldli << std::endl;
 
     Tval ratio = ApproxCholValidation(A, ldli, 1e-4);
 
