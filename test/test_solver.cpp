9c79ee7d630bf0d6c9042ec07cdfa27ca1df324b
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-18 21:02:50 +0200

    Minor.

--- test/test_solver.cpp
+++ test/test_solver.cpp
@@ -3,17 +3,18 @@
 #include "test.h"
 #include "matrix.hpp"
 
-
 int main(int argc, char **argv){
 
     int v = atoi(argv[1]);
     int e = atoi(argv[2]);
 
     SparseMatrix A(v, e);
+
     LLMatOrd llmat = LLMatOrd(A);
     std::cout << "Created random sparse matrix A.\n";
 
-    approxChol(llmat);
+    // std::cout << "Run approxChol once.\n"
+    // approxChol(llmat);
 
     return 0;
 
