fbd183f834629a50ad2e020bf5ba3053de6ef154
Fanlin Wang <fanwang@student.ethz.ch>
2020-05-21 11:24:09 +0000

    Some scripts.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-vertice=(10 100 1000 2000 5000 8000 10000)
-edge=(100 500 1000 2000 5000 8000 10000 20000)
+vertice=(10 100 1000 5000 10000 20000)
+edge=(100 500 1000 5000 10000 20000 40000)
 
 for V in ${vertice[@]}
 do
@@ -12,26 +12,7 @@ do
             # to get exact cycles, run main without perf. 
             ./build/src/main $V $E | tee -a "./log/exact_cycle.txt"
 
-            # to get general measures from perf
-            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/general_metrics.txt"
-            3>>"./log/general_metrics.txt" perf stat --log-fd 3 --append -d ./build/test/test_solver $V $E | tee -a "./log/general_metrics.txt"
-
-            # to get cache loads from perf
-            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/cache_log.txt"
-            3>>"./log/cache_log.txt" perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,iTLB-loads,iTLB-load-misses --log-fd 3 --append -d ./build/test/test_solver $V $E | tee -a "./log/cache_log.txt"
-
-            # to get flops counts from perf
-            echo "Start test. V = ${V}, E = ${E}" | tee -a "./log/flops_log.txt"
-            3>>"./log/flops_log.txt" perf stat -e r5301c7,r5302c7 --log-fd 3 --append -d ./build/test/test_solver $V $E | tee -a "./log/flops_log.txt"
-
-            if [[ $1 == 'p' ]] 
-            then
-                perf record -o "./log/bin/perf_v${V}_e${E}.data" ./build/src/main $V $E
-                perf report --stdio --dsos=main -i "./log/bin/perf_v${V}_e${E}.data" | tee -a "./log/report_main/pf_v${V}_e${E}.txt"
-                perf annotate --stdio --dsos=main -i "./log/bin/perf_v${V}_e${E}.data"  | tee -a "./log/annotate_main/p_v${V}_e${E}.txt"
-                1>>"./log/annotate_approxChol/perf_annotate_v${V}_e${E}.txt" perf annotate --stdio --dsos=main --symbol=approxChol -i "./log/bin/perf_v${V}_e${E}.data"  
-            fi
-        fi
+         fi
     done
 done
 
